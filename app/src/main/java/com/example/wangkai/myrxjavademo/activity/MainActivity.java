c4b448a84c99923cf38fcf0ccc729719432a58ce
王凯 <xiaokai@maihaoche.com>
2016-07-13 11:11:48 +0800

    RxJava+Retrofit

--- app/src/main/java/com/example/wangkai/myrxjavademo/activity/MainActivity.java
+++ app/src/main/java/com/example/wangkai/myrxjavademo/activity/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.wangkai.myrxjavademo.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.example.wangkai.myrxjavademo.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    
 }
